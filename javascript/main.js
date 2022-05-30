f37579102037e0ff1fbb8fe1759bd2fe8be96597
nguyensangautocad2018 <106513733+nguyensangautocad2018@users.noreply.github.com>
2022-05-30 21:01:55 +0700

    third commit

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,11 @@
 let searchForm = document.querySelector(".search-form");
 
-document.querySelector("#search-btn").onclick = () => {
+document.querySelector("#search-btn").onclick = (e) => {
     searchForm.classList.toggle('active');
+}
+
+let cart = document.querySelector(".shopping-cart");
+
+document.querySelector("#cart-btn").onclick = (e) => {
+    cart.classList.toggle('active');
 }
